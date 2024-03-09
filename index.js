3f6294aadd914330425b50d82fe0757023386efc
JulianaAmoasei <late.parrot@gmail.com>
2024-03-09 20:50:07 -0300

    video 1.4

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+
 const caminho = process.argv;
 const link = caminho[2]; 
-console.log(link);
+
+fs.readFile(link, 'utf-8', (erro, result) => {
+  console.log(result)
+});
