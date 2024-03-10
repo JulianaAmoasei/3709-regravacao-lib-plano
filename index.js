21729f910790d6671dd8239e165291e4cb654535
JulianaAmoasei <late.parrot@gmail.com>
2024-03-10 09:40:05 -0300

    video 2.1

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ const caminho = process.argv;
 const link = caminho[2]; 
 
 fs.readFile(link, 'utf-8', (erro, result) => {
-  console.log(result)
+  verificaPalavrasDuplicadas(result)
 });
+
+function verificaPalavrasDuplicadas(texto){
+  const listaPalavras = texto.split(" ")
+  const resultado = {}
+  listaPalavras.forEach( word => {
+    // const cleanWord = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    // if (word.length >= 3){
+      resultado[word] = (resultado[word] || 0) +1
+    // }
+  })
+  console.log(resultado);
+  // imprimeResultado(resultado);
+}
+
+// function imprimeResultado(resultado) {
+//   console.log(resultado);
+// }
