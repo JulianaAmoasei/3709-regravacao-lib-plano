01450fabff6168e24c65c477dcbd49e8e3fbdd13
JulianaAmoasei <late.parrot@gmail.com>
2024-03-10 17:05:44 -0300

    video 3.2

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 const fs = require("fs");
 
 const caminho = process.argv;
-const link = caminho[2]; 
+const link = caminho[2];
 
-fs.readFile(link, 'utf-8', (erro, result) => {
-  if (erro) {
-    console.log('qual foi o erro?', erro.code)
-    return
+fs.readFile(link, "utf-8", (erro, result) => {
+  try {
+    if (erro) throw erro
+    contaPalavras(result);
+  } catch (erro) {
+    if (erro.code === "ENOENT") console.log('erro que esperava');
+    else console.log('outro erro?');
   }
-  contaPalavras(result)
- });
- 
+});
 
 function contaPalavras(texto) {
   const paragrafos = extraiParagrafos(texto);
   const contagem = paragrafos.flatMap((paragrafo) => {
-    if (!paragrafo) return []
-    return verificaPalavrasDuplicadas(paragrafo)
-  })
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  });
   console.log(contagem);
 }
 
 function extraiParagrafos(texto) {
-  return texto.toLowerCase().split("\n")
+  return texto.toLowerCase().split("\n");
 }
 
 function limpaPalavra(palavra) {
-  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 }
 
-function verificaPalavrasDuplicadas(texto){
-  const listaPalavras = texto.split(" ")
-  const resultado = {}
-  listaPalavras.forEach(word => {
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+  listaPalavras.forEach((word) => {
     if (word.length >= 3) {
-    const palavraLimpa = limpaPalavra(word)
-      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) +1
+      const palavraLimpa = limpaPalavra(word);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
     }
-  })
+  });
   return resultado;
 }
 
